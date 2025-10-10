f4dceca94e469b4f315828d26b35dcec5197fb00
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2025-10-10 19:33:50 -0300

    Base Código base para desenvolver o trabalho. As partes foram devidamente comentadas no código.

--- src/ricardo_paulo/net/Main.java
+++ src/ricardo_paulo/net/Main.java
@@ -1,7 +1,57 @@
 package ricardo_paulo.net;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Escolha uma branch para visualizar a atividade e, se houver, suas respectivas questões.");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Bem vindo ao sistema de registro de alunos!");
+        System.out.println("Escolha uma operação abaixo: ");
+
+        boolean exit = false;
+
+        do {
+            System.out.println("1 - Cadastro\n2 - Consulta\n3 - Edição\n4 - Exclusão\n0 - Sair");
+            int option = scan.nextInt();
+
+            switch (option) {
+                case 1: register();
+                    break;
+                case 2: System.out.println(search());
+                    break;
+                case 3: update();
+                    break;
+                case 4: delete();
+                    break;
+                case 0: exit = true;
+                    break;
+
+                default: System.out.println("Insira uma opção válida!");
+            }
+        } while (!exit);
+
+        System.out.println("Programa finalizado.");
+    }
+
+    // Paulo
+    private static void register () {
+
+    }
+
+    // Paulo
+    private static String search () {
+
+        return "";
+    }
+
+    // Arthur
+    private static void update () {
+
+    }
+
+    // Jackson
+    private static void delete () {
+
     }
 }
