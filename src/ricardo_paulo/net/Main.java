3b9efa12f6a59bbd5d77b9ee393a341e866673c6
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2025-10-11 18:53:42 -0300

    Classe estudante adicionada

--- src/ricardo_paulo/net/Main.java
+++ src/ricardo_paulo/net/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
+
+        Student[] students;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Bem vindo ao sistema de registro de alunos!");
---
+++ src/ricardo_paulo/net/Student.java
@@ -0,0 +1,78 @@
+package ricardo_paulo.net;
+
+import java.security.InvalidParameterException;
+
+public class Student {
+
+    private final String registry;
+    private String name;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private double[] notes;
+
+    public Student(String registry, String name, String address, String email, String phoneNumber, double[] notes) {
+        this.registry = registry;
+        this.name = name;
+        this.address = address;
+        setEmail(email);
+        setPhoneNumber(phoneNumber);
+        this.notes = notes;
+    }
+
+    public String getRegistry() {
+        return registry;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email.contains("@") && email.contains(".")) {
+            this.email = email;
+        } else {
+            throw new InvalidParameterException("Insira um endereço de email válido!");
+        }
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() == 11) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw new InvalidParameterException("Insira um número de telefone válido!");
+        }
+    }
+
+    public double[] getNotes() {
+        return notes;
+    }
+
+    public void setNotes(double[] notes) {
+
+
+//        if () {
+//            this.notes = notes;
+//        }
+    }
+}
