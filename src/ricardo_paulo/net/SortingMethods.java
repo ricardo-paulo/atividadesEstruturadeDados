4407a179fc57585fb041ee3182cf4643e20bbb45
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2025-10-12 11:09:16 -0300

    Adicionado tratamento de erros a Sorting Method

--- src/ricardo_paulo/net/SortingMethods.java
+++ src/ricardo_paulo/net/SortingMethods.java
@@ -6,17 +6,23 @@ public class SortingMethods {
         Student[] arr = students.clone();
         int n = arr.length;
 
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
+        try {
+            for (int i = 0; i < n - 1; i++) {
+                int minIndex = i;
 
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j].getRegistry() < arr[minIndex].getRegistry())
-                    minIndex = j;
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j].getRegistry() < arr[minIndex].getRegistry())
+                        minIndex = j;
+                }
+
+                Student temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
             }
-
-            Student temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
+        } catch (Exception e) {
+            if (e.getCause() == null) {
+                return arr;
+            }
         }
 
         return arr;
